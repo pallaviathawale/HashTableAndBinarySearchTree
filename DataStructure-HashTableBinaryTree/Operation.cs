789bf1ee7be8db1e1de95b075ab866c350a2e3f7
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-23 18:01:48 +0530

    [PALLAVI] Add: FrequencyInParagraph

--- DataStructure-HashTableBinaryTree/Operation.cs
+++ DataStructure-HashTableBinaryTree/Operation.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace DataStructure_HashTableBinaryTree
 {
-    class Operation
-    {
-        Dictionary<string, int> frequency = new Dictionary<string, int>();
-        public void Frequency(MyMapNode<int, string> hash)
+  class Operation
         {
-            for (int key = 0; key < hash.size; key++)
+            Dictionary<string, int> frequency = new Dictionary<string, int>();
+            public void Frequency(MyMapNode<int, string> hash)
             {
-                frequency.TryAdd(hash.Get(key).ToLower(), 0);
-                frequency[hash.Get(key).ToLower()]++;
-            }
-            foreach (KeyValuePair<string, int> item in frequency)
-            {
-                Console.WriteLine($"frequency of word '{item.Key}' is {item.Value}");
+                for (int key = 0; key < hash.size; key++)
+                {
+                    frequency.TryAdd(hash.Get(key).ToLower(), 0);
+                    frequency[hash.Get(key).ToLower()]++;
+                }
+                foreach (KeyValuePair<string, int> item in frequency)
+                {
+                    Console.WriteLine($"frequency of word '{item.Key}' is {item.Value}");
+                }
             }
         }
-    }
 }
