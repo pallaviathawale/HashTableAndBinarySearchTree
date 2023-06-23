d9c62f14a5c950beb7391b3b98f897df74e5b291
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-23 17:55:23 +0530

    [PALLAVI] Add: File

---
+++ DataStructure-HashTableBinaryTree/Operation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_HashTableBinaryTree
+{
+    class Operation
+    {
+        Dictionary<string, int> frequency = new Dictionary<string, int>();
+        public void Frequency(MyMapNode<int, string> hash)
+        {
+            for (int key = 0; key < hash.size; key++)
+            {
+                frequency.TryAdd(hash.Get(key).ToLower(), 0);
+                frequency[hash.Get(key).ToLower()]++;
+            }
+            foreach (KeyValuePair<string, int> item in frequency)
+            {
+                Console.WriteLine($"frequency of word '{item.Key}' is {item.Value}");
+            }
+        }
+    }
+}
